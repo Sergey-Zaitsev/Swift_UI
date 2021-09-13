29277ff950d6e7abf42abd9055f3ddac1bf2504c
Sergey-Zaitsev <73549064+Sergey-Zaitsev@users.noreply.github.com>
2021-09-13 09:04:49 +0300

    Add Lesson1

---
+++ vc_Lesson1/vc_Lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  vc_Lesson1App.swift
+//  vc_Lesson1
+//
+//  Created by Сергей Зайцев on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct vc_Lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
